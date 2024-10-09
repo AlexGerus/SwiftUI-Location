440dfeb1c095b3bb52969132a081cc14de0af6b4
Alex Gerus <alexander.gerus@grse.de>
2024-10-09 14:03:32 +0300

    Init

---
+++ SwiftUI-Location/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  SwiftUI-Location
+//
+//  Created by Alexander Gerus on 04.04.2023.
+//
+
+import CoreLocationUI
+import MapKit
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject private var viewModel = ContentViewModel()
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
+                .ignoresSafeArea()
+                .tint(.pink)
+            
+            LocationButton(.currentLocation) {
+                viewModel.requestAllowOnceLocationPermission()
+            }
+            .foregroundColor(.white)
+            .cornerRadius(8)
+            .labelStyle(.titleAndIcon)
+            .symbolVariant(.fill)
+            .tint(.pink)
+            .padding(.bottom, 50)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+final class ContentViewModel:
+    NSObject, ObservableObject, CLLocationManagerDelegate {
+    @Published var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 40,
+            longitude: 120),
+        span: MKCoordinateSpan(
+            latitudeDelta: 100,
+            longitudeDelta: 100))
+    
+    let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+    }
+    
+    func requestAllowOnceLocationPermission() {
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]) {
+        guard let latestLocation = locations.first else {
+            // show an error
+            return
+        }
+        
+        DispatchQueue.main.async {
+            self.region = MKCoordinateRegion(
+                center: latestLocation.coordinate,
+                span: MKCoordinateSpan(
+                    latitudeDelta: 0.05,
+                    longitudeDelta: 0.05))
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+}
---
+++ SwiftUI-Location/SwiftUI_LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LocationApp.swift
+//  SwiftUI-Location
+//
+//  Created by Alexander Gerus on 04.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
